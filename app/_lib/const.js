03d3f2016f3db21ab5ff382e8ebe46526c80f451
ervin-sungkono <ervinsung@gmail.com>
2025-04-18 21:46:13 +0700

    feat: add navbar component

---
+++ app/_lib/const.js
@@ -0,0 +1,30 @@
+import { IoMdHome as HomeIcon, IoMdAdd as PlusIcon, IoMdSettings as SettingIcon } from "react-icons/io";
+import { IoList as ListIcon, IoStorefront as StoreIcon } from "react-icons/io5";
+
+// navbar items
+export const NAV_ITEMS = [
+    {
+        label: 'Home',
+        icon: <HomeIcon size={24}/>,
+        url: '/home'
+    },
+    {
+        label: 'Expenses',
+        icon: <ListIcon size={24}/>,
+        url: '/expenses'
+    },
+    {
+        label: 'Add Expense',
+        icon: <PlusIcon size={28}/>
+    },
+    {
+        label: 'Shops',
+        icon: <StoreIcon size={24}/>,
+        url: '/shops'
+    },
+    {
+        label: 'Settings',
+        icon: <SettingIcon size={24}/>,
+        url: '/settings'
+    }
+]
