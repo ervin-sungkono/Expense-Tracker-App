0316636e9594a445f9b660324489cd0ec90b1273
Ervin <ervinsung@gmail.com>
2025-04-20 13:43:43 +0700

    feat: add register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,9 +1,35 @@
+'use client'
 import Layout from "../_components/layout/Layout";
+import TextContent from "../_components/common/TextContent";
+import Button from "../_components/common/Button";
+import InputField from "../_components/common/InputField";
+import { createRef, useEffect } from "react";
 
 export default function Register() {
-    return(
-        <Layout pathname={'/register'}>
+    const inputRef = createRef();
+    useEffect(() => {
+        console.log(inputRef.current);
+    }, [inputRef])
 
+    return(
+        <Layout pathname={'/register'} hideNavbar>
+            <div className="h-full flex flex-col justify-center gap-4">
+                <TextContent
+                    title={"Get Started"}
+                    description={"To start using our app, please fill in the field below."}
+                />
+                <InputField
+                    ref={inputRef}
+                    name={"username"}
+                    label={"Your Name"}
+                    required
+                    placeholder={"Enter your name"}
+                />
+            </div>
+            <Button 
+                label="Let's Go" 
+                className="absolute bottom-8 left-0 px-14"
+            />
         </Layout>
     )
 }
