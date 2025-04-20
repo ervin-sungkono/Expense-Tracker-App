f2e2d2471ee03517ec1b34e7b0789ae904d8175e
Ervin <ervinsung@gmail.com>
2025-04-20 12:16:16 +0700

    feat: add required page routes

---
+++ app/expenses/page.js
@@ -0,0 +1,7 @@
+import Layout from "../_components/layout/Layout";
+
+export default function Expenses() {
+    return(
+        <Layout pathname={"/expenses"}>Expenses</Layout>
+    )
+}
---
+++ app/register/page.js
@@ -0,0 +1,9 @@
+import Layout from "../_components/layout/Layout";
+
+export default function Register() {
+    return(
+        <Layout pathname={'/register'}>
+
+        </Layout>
+    )
+}
