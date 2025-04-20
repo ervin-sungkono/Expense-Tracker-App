056b7413711ed3ccbd54ca3a32fd48f8d69128f6
Ervin <ervinsung@gmail.com>
2025-04-20 13:12:14 +0700

    fix: fix breakpoint style

--- app/_components/common/TextContent.js
+++ app/_components/common/TextContent.js
@@ -5,12 +5,12 @@ export default function TextContent({ title, description }) {
 
     return(
         <div className="flex flex-col gap-2">
-            {title && <h2 className="text-3xl xs:text-2xl text-dark dark:text-white font-bold">
+            {title && <h2 className="text-2xl xs:text-3xl text-dark dark:text-white font-bold">
                 {titleContent.map((part, index) => (
                     <span key={index} className={part.type === 'tag' ? 'text-ocean-blue' : ''}>{part.value}</span>
                 ))}
             </h2>}
-            {description && <p className="text-dark/80 dark:text-white/80 text-base xs:text-sm">{description}</p>}
+            {description && <p className="text-dark/80 dark:text-white/80 text-sm xs:text-base">{description}</p>}
         </div>
     )
 }
