738a6c7ced2f22fe31662149de8ac1e59fb5ec1a
Ervin <ervinsung@gmail.com>
2025-04-23 22:08:59 +0700

    fix: add `type` prop on Button component

--- app/_components/common/Button.js
+++ app/_components/common/Button.js
@@ -3,6 +3,7 @@
 export default function Button({ 
     label, 
     className = '', 
+    type='button',
     size = 'medium', 
     onClick = null, 
     contained = false 
@@ -19,7 +20,7 @@ export default function Button({
     return(
         <div className={`${contained ? 'w-fit' : 'w-full'} ${className}`}>
             <div className="relative w-full">
-                <button onClick={onClick} className={`relative cursor-pointer w-full flex justify-center items-center ${getSizeClass()} bg-basic-gradient rounded-full font-semibold z-fixed hover:bg-basic-gradient--hover hover:scale-[1.04] active:scale-95 transition-all duration-150 ease-in-out`}>
+                <button type={type} onClick={onClick} className={`relative cursor-pointer w-full flex justify-center items-center ${getSizeClass()} bg-basic-gradient rounded-full font-semibold z-fixed hover:bg-basic-gradient--hover hover:scale-[1.04] active:scale-95 transition-all duration-150 ease-in-out`}>
                     {label}
                 </button>
                 <span className="absolute w-4/5 h-4/5 bg-basic-gradient left-1/2 bottom-0 -translate-x-1/2 translate-y-1/4 rounded-full blur-[48px]"></span>
