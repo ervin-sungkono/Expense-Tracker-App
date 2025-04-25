e92ce9c90ce1b79c5f4ed4eaf7d6cb5fb4efb4c8
Ervin <ervinsung@gmail.com>
2025-04-25 21:29:19 +0700

    feat: develop home header

--- app/home/page.js
+++ app/home/page.js
@@ -1,7 +1,10 @@
 import Layout from "../_components/layout/Layout";
+import Header from "../_components/home/Header";
 
 export default function Home() {
     return(
-        <Layout pathname={"/home"}>Home</Layout>
+        <Layout pathname={"/home"}>
+            <Header/>
+        </Layout>
     )
 }
