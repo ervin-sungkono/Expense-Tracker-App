63135d52d26c143a8d81989a1f7436d6991d6d2e
Ervin <ervinsung@gmail.com>
2025-04-29 18:17:15 +0700

    fix: adjust styling on home page

--- app/_components/home/SubHeader.js
+++ app/_components/home/SubHeader.js
@@ -3,16 +3,16 @@ import { IoChevronForward as RightIcon } from "react-icons/io5";
 
 export default function SubHeader({ title, description, link }) {
     return(
-        <div className="w-full flex items-center gap-2.5 mb-3">
-            <div className="flex flex-col gap-0.5 grow">
-                <p className="text-lg font-semibold text-dark dark:text-white">{title}</p>
-                { description && <p className="text-xs md:text-sm text-dark/80 dark:text-white/80">{description}</p>}
+        <div className="w-full flex flex-col gap-0.5 mb-3">
+            <div className="flex items-center gap-2.5">
+                <p className="grow text-lg font-semibold text-dark dark:text-white">{title}</p>
+                {link && 
+                <Link href={link} className="flex items-center gap-1 text-deep-blue dark:text-ocean-blue">
+                    <p className="text-sm">Manage</p>
+                    <RightIcon size={20}/>
+                </Link>}
             </div>
-            {link && 
-            <Link href={link} className="flex items-center gap-1 text-deep-blue dark:text-ocean-blue">
-                <p className="text-sm">Manage</p>
-                <RightIcon size={20}/>
-            </Link>}
+            { description && <p className="block text-xs md:text-sm text-dark/80 dark:text-white/80">{description}</p>}
         </div>
     )
 }
