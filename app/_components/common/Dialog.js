6d555a8578105ae79351d8c98278b45bf1dce941
Ervin <ervinsung@gmail.com>
2025-05-03 21:14:46 +0700

    fix: fix dialog overflow

--- app/_components/common/Dialog.js
+++ app/_components/common/Dialog.js
@@ -18,13 +18,13 @@ export default function Dialog({ children, show = false, hideFn = null }) {
     if(!hidden)
     return(
         <div className="fixed w-full h-full px-6 py-8 top-0 left-0 flex justify-center items-center">
-            <div className={`relative w-full max-h-full max-w-2xl bg-white dark:bg-neutral-700 rounded-lg px-6 py-8 z-50 ${show ? 'animate-[show_.5s_forwards_ease-in-out]' : 'animate-[hide_.5s_forwards_ease-in-out]'}`}>
+            <div className={`relative w-full overflow-y-auto max-h-full max-w-2xl bg-white dark:bg-neutral-700 rounded-lg px-6 py-8 z-50 ${show ? 'animate-[show_.5s_forwards_ease-in-out]' : 'animate-[hide_.5s_forwards_ease-in-out]'}`}>
                 {children}
-                <button className="absolute cursor-pointer p-2 bg-basic-gradient rounded-full right-0 top-0 -translate-y-1/3 translate-x-1/3 hover:scale-105 active:scale-95 transition-transform duration-150 ease-in-out" onClick={hideFn}>
+                <button className="absolute cursor-pointer p-2 bg-basic-gradient rounded-bl-lg right-0 top-0" onClick={hideFn}>
                     <IoMdClose size={20}/>
                 </button>
             </div>
-            <div className="overlay absolute bg-dark/60 w-full h-full"></div>
+            <div className="pointer-events-none overlay absolute bg-dark/60 w-full h-full"></div>
         </div>
     )
 }
