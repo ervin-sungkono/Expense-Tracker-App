65a9b1de667d69bcf5f6d7f68a35f7795a840119
Ervin <ervinsung@gmail.com>
2025-05-17 18:42:29 +0700

    fix: expense card styling

--- app/_components/expenses/ExpenseCard.js
+++ app/_components/expenses/ExpenseCard.js
@@ -44,7 +44,7 @@ function ExpenseCard({ expense, style }) {
             <div {...handlers()} onClick={!isMobile ? () => setShowMenu(true) : null} className="cursor-pointer px-4 py-2 flex active:bg-neutral-300/30 active:dark:bg-neutral-800/30 transition-colors duration-150 ease-in-out">
                 <div className="w-full flex flex-col gap-1">
                     <div className="w-full flex items-center gap-1.5">
-                        <p className="text-base md:text-lg font-medium grow">{category}</p>
+                        <p className="text-base font-medium grow">{category}</p>
                         <p className="text-sm md:text-base font-medium">{formatCurrency(amount)}</p>
                     </div>
                     <p className="text-xs text-dark/80 dark:text-white/80">{formatDateString(date)}</p>
