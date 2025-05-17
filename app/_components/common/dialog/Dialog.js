296a96b557f337b2be6d7ac3bc22c96462dfcb80
Ervin <ervinsung@gmail.com>
2025-05-17 09:42:35 +0700

    fix: dialog style

--- app/_components/common/dialog/Dialog.js
+++ app/_components/common/dialog/Dialog.js
@@ -18,7 +18,7 @@ export default function Dialog({ children, show = false, hideFn = null, hideCanc
     if(!hidden)
     return(
         <div className="fixed w-full h-full px-4 py-8 top-0 left-0 flex justify-center items-center z-[10000]">
-            <div className={`relative w-full overflow-y-auto max-h-full max-w-2xl bg-white dark:bg-neutral-700 rounded-lg px-6 py-8 z-50 ${show ? 'animate-[show_.5s_forwards_ease-in-out]' : 'animate-[hide_.5s_forwards_ease-in-out]'}`}>
+            <div className={`relative w-full overflow-y-auto max-h-full max-w-2xl bg-white dark:bg-neutral-700 rounded-lg px-6 pt-9 pb-8 z-50 ${show ? 'animate-[show_.5s_forwards_ease-in-out]' : 'animate-[hide_.5s_forwards_ease-in-out]'}`}>
                 {children}
                 {!hideCancelButton && 
                 <button className="absolute cursor-pointer p-2 bg-basic-gradient rounded-bl-lg right-0 top-0" onClick={hideFn}>
