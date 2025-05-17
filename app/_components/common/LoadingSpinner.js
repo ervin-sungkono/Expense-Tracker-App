dfd86e4d7a217f38fbb1967616f171cf23f32846
Ervin <ervinsung@gmail.com>
2025-05-17 18:30:50 +0700

    feat: add loading spinner for list

---
+++ app/_components/common/LoadingSpinner.js
@@ -0,0 +1,7 @@
+'use client'
+
+export default function LoadingSpinner() {
+    return (
+         <div className="w-24 h-24 md:w-36 md:h-36 border-12 md:border-18 rounded-full border-ocean-blue/50 border-t-ocean-blue left-1/4 right-1/4 top-1/4 bottom-1/4 animate-[rotate-spinner_1s_linear_infinite]"></div>
+    )
+}
