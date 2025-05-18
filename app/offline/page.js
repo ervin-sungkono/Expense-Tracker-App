b57592adb9c25dd3df76b47864b824db14c9ef46
Ervin <ervinsung@gmail.com>
2025-05-18 10:16:37 +0700

    fix: add styling for offline page

--- app/offline/page.js
+++ app/offline/page.js
@@ -1,3 +1,14 @@
+import Layout from "../_components/layout/Layout";
+import { MdOutlineWifiOff as OfflineIcon } from "react-icons/md";
+
 export default function Offline() {
-    return <>You are Offline</>;
+    return (
+        <Layout hideNavbar>
+            <div className="flex flex-col gap-2 h-full justify-center items-center text-center">
+                <OfflineIcon size={64}/>
+                <p className="text-2xl md:text-3xl font-bold text-dark dark:text-white">You are offline</p>
+                <p className="text-sm md:text-base text-dark/80 dark:text-white/80">Please check your internet connection and try again.</p>
+            </div>
+        </Layout>
+    );
 }
