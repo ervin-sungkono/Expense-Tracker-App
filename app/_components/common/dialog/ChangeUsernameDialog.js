fee7d960b5c54c970d8f42a4c42ab7b488db2691
Ervin <ervinsung@gmail.com>
2025-05-18 11:48:03 +0700

    feat: implement change username

---
+++ app/_components/common/dialog/ChangeUsernameDialog.js
@@ -0,0 +1,77 @@
+'use client'
+import Dialog from "./Dialog";
+import InputField from "../InputField";
+import { useState } from "react";
+import Button from "../Button";
+import { StringValidator } from "@/app/_lib/validator";
+import { useLocalStorage } from "@/app/_lib/hooks";
+
+export default function ChangeUsernameDialog({ show, hideFn }) {
+    const [errorMessage, setErrorMessage] = useState({});
+    const [username, setUsername] = useLocalStorage('username');
+
+    const validateName = (name) => {
+        return new StringValidator("Name", name)
+            .required()
+            .minLength(3)
+            .maxLength(30)
+            .validate();
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+
+        // Create payload for add expense
+        const payload = {}
+        for(const [key, value] of formData.entries()) {
+            payload[key] = value;
+        }
+
+        try{
+            let error = {};
+            payload.budget = Number(payload.budget); // ensure that value stored is Number type
+            const { name } = payload;
+
+            error.name = validateName(name);
+
+            setErrorMessage(error);
+            if(Object.values(error).filter(Boolean).length > 0) {
+                return;
+            }
+
+            setUsername(name)
+            form.reset();
+            hideFn && hideFn();
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <Dialog 
+            show={show} 
+            hideFn={hideFn}
+        >
+            <div className="flex flex-col gap-4">
+                <div className="text-xl font-bold">{'Change Username'}</div>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-4 mb-6">
+                        <InputField 
+                            required
+                            name={"name"} 
+                            label={"New Username"} 
+                            placeholder={"Enter new username"}
+                            type={"text"}
+                            defaultValue={username}
+                            errorMessage={errorMessage?.name}
+                        />
+                    </div>
+                    <Button type="submit" label={'Update'}/>
+                </form>
+            </div>
+        </Dialog>
+    )
+}
