10e23e4bd923e8a0025f6bacd027e0098c645e34
Ervin <ervinsung@gmail.com>
2025-05-18 20:58:16 +0700

    fix: adjust page animation duration

--- app/_components/common/Page.js
+++ app/_components/common/Page.js
@@ -7,7 +7,7 @@ export default function Page({ children, title, show = false, hideFn = null, hid
 
     useEffect(() => {
         let hide = null;
-        if(!show && !hidden) hide = setTimeout(() => setHidden(true), 500);
+        if(!show && !hidden) hide = setTimeout(() => setHidden(true), 400);
         else if(show && hidden) {
             setHidden(false)
         }
@@ -18,7 +18,7 @@ export default function Page({ children, title, show = false, hideFn = null, hid
     if(!hidden)
     return(
         <div className="fixed w-full h-full top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[10000]">
-            <div className={`relative w-full h-full flex flex-col overflow-y-auto max-w-2xl bg-white dark:bg-neutral-700 px-6 pb-8 z-50 ${show ? 'animate-[slide-in_.5s_forwards_ease-in-out]' : 'animate-[slide-out_.5s_forwards_ease-in-out]'}`}>
+            <div className={`relative w-full h-full flex flex-col overflow-y-auto max-w-2xl bg-white dark:bg-neutral-700 px-6 pb-8 z-50 ${show ? 'animate-[slide-in_.4s_forwards_ease-in-out]' : 'animate-[slide-out_.4s_forwards_ease-in-out]'}`}>
                 <div className="relative h-14 md:h-16 flex gap-4 justify-center items-center">
                     {!hideBackButton && 
                     <div onClick={hideFn} className="absolute left-0 cursor-pointer select-none p-2.5 rounded-full active:bg-neutral-300/30 dark:active:bg-neutral-800/30">
