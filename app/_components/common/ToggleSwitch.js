f71de1d4103e94ab1938d5e6124261952f611180
Ervin <ervinsung@gmail.com>
2025-05-18 11:37:08 +0700

    feat: add toggle switch component

---
+++ app/_components/common/ToggleSwitch.js
@@ -0,0 +1,26 @@
+'use client'
+import { useEffect, useState } from "react"
+
+export default function ToggleSwitch({ icon, switchStatus = false, onStatusChange }) {
+    const [status, setStatus] = useState(switchStatus);
+
+    const toggle = (e) => {
+        setStatus(!status);
+    };
+
+    useEffect(() => {
+        onStatusChange && onStatusChange(status);
+    }, [status])
+
+    useEffect(() => {
+        setStatus(switchStatus);
+    }, [switchStatus])
+
+    return(
+        <div onClick={toggle} className={`relative wrapper w-12 h-7 md:w-14 md:h-8 p-1 ${status ? 'bg-ocean-blue' : 'bg-neutral-600'} rounded-full transition-colors duration-200 ease-in-out`}>
+            <div className={`absolute ${status ? 'translate-x-full' : 'translate-x-0'} flex justify-center items-center circle w-5 h-5 md:w-6 md:h-6 bg-neutral-700 rounded-full transition-transform duration-200 ease-in-out`}>
+                {icon}
+            </div>
+        </div>
+    )
+}
