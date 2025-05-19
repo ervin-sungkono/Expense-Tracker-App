39b61bb8efaadb2984695ba419f7d369109f6adc
Ervin <ervinsung@gmail.com>
2025-05-19 21:09:09 +0700

    fix: theme provider props adjust

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider enableColorScheme enableSystem>
           {children}
         </ThemeProvider>
       </body>
