ef93f101430c0bf360c0aaf31942457b9d685f64
Ervin <ervinsung@gmail.com>
2025-06-01 18:58:55 +0700

    feat: category list based on type

--- app/_components/common/CategoryList.js
+++ app/_components/common/CategoryList.js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import VirtualizedCategoryList from "../categories/VirtualizedCategoryList";
 import Button from "./Button";
 import Page from "./Page";
+import CategoryTab from "../categories/CategoryTab";
 import AddCategoryDialog from "../categories/AddCategoryDialog";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "@/app/_lib/db";
@@ -10,40 +11,50 @@ import { db } from "@/app/_lib/db";
 export default function CategoryList({ show, hideFn }) {
     const [showDialog, setShowDialog] = useState(false);
     const [categoryData, setCategoryData] = useState(null);
+    const [selectedType, setSelectedType] = useState('Expense');
     const categories = useLiveQuery(() => db.getAllCategories());
     const categoryRef = useRef(null);
 
     useEffect(() => {
         if(categories) {
-            const result = {}
+            const result = {
+                'Expense': {},
+                'Income': {},
+                'DebtLoan': {}
+            }
             
             categories.forEach(category => {
                 if(!category.parentId) {
-                    result[category.id] = {
+                    result[category.type][category.id] = {
                         ...category,
-                        data: result[category.id]?.data ?? []
+                        data: result[category.type][category.id]?.data ?? []
                     }
                 } else {
-                    if(!result[category.parentId]) {
-                        result[category.parentId] = {
+                    if(!result[category.type][category.parentId]) {
+                        result[category.type][category.parentId] = {
                             data: [category]
                         }
                     } else {
-                        result[category.parentId].data.push(category);
+                        result[category.type][category.parentId].data.push(category);
                     }
                 }
             })
+            
+            const types = Object.keys(result);
+            types.forEach(type => {
+                result[type] = Object.values(result[type]);
+            })
 
-            setCategoryData(Object.values(result));
+            setCategoryData(result);
         }
     }, [categories])
 
     useEffect(() => {
-        if(categoryData) {
+        if(categoryData && categoryData[selectedType]) {
             console.log('TRIGGER ITEM SIZE RECOMPUTE');
             categoryRef.current?.resetAfterIndex(0);
         }
-    }, [categoryData])
+    }, [categoryData, selectedType])
 
     return(
         <Page
@@ -51,14 +62,15 @@ export default function CategoryList({ show, hideFn }) {
             show={show}
             hideFn={hideFn}
         >
-            <div className="grow flex flex-col gap-2">
+            <div className="grow flex flex-col">
+                <CategoryTab selected={selectedType} onChange={(type) => setSelectedType(type)}/>
                 <div className="grow">
                     <VirtualizedCategoryList
                         ref={categoryRef}
-                        items={categoryData}
+                        items={categoryData?.[selectedType]}
                     />
                 </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center mt-2">
                     <Button label={"Add New Category"} onClick={() => setShowDialog(true)}/>
                 </div>
                 <AddCategoryDialog
