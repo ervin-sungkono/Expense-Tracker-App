01c0a8a76bfb79c5ce3e4fb36aeaefe0f12a542a
Ervin <ervinsung@gmail.com>
2025-06-01 15:45:41 +0700

    fix: list component overflow

--- app/_components/common/List.js
+++ app/_components/common/List.js
@@ -3,7 +3,7 @@ import ListItem from "./ListItem";
 export default function List({ items = [] }) {
     if(items.length === 0) return;
     return(
-        <div className="py-2 bg-neutral-200 dark:bg-neutral-700 rounded-lg overflow-hidden">
+        <div className="py-2 bg-neutral-200 dark:bg-neutral-700 rounded-lg">
             {items.map(item => (
                 <ListItem key={item.id} {...item}/>
             ))}
