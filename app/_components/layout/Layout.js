c185fa6e516b5c14509748737bc6bde243cafee6
Ervin <ervinsung@gmail.com>
2025-06-01 15:45:32 +0700

    fix: slightly adjust layout margin to match navbar

--- app/_components/layout/Layout.js
+++ app/_components/layout/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children, hideNavbar = false, pathname }) {
     return(
         <div className="flex flex-col overflow-auto fixed top-0 left-0 bottom-0 right-0">
             { !hideNavbar && <Navbar items={NAV_ITEMS} pathname={pathname}/> }
-            <main className={`relative w-full h-full overflow-x-hidden overflow-y-auto max-w-2xl px-6 pt-8 ${hideNavbar ? 'pb-8' : 'mb-24'} mx-auto`}>
+            <main className={`relative w-full h-full overflow-x-hidden overflow-y-auto max-w-2xl px-6 pt-8 ${hideNavbar ? 'pb-8' : 'mb-22'} mx-auto`}>
                 {children}
             </main>
         </div>
