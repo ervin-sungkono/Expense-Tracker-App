84a3f7a213668935452b45043fa4e9a8b708a93f
Ervin <ervinsung@gmail.com>
2025-06-08 20:05:32 +0700

    fix: adjust navbar item content

--- app/_components/common/navbar/NavbarItem.js
+++ app/_components/common/navbar/NavbarItem.js
@@ -3,12 +3,10 @@ import Link from "next/link";
 export default function NavbarItem({ item, active = false }) {
     if (!item) return
     return(
-        <div  
-            className={`nav-link`}
-            aria-label={item.label}
-        >
-            <Link scroll={false} href={item.url} className={`text-dark${active ? ' nav-active' : ''}`}>
-                {item.icon}
+        <div className={`nav-link`}>
+            <Link scroll={false} href={item.url} className={`${active ? ' nav-active' : ''}`} aria-label={item.label}>
+                <div aria-hidden="true">{item.icon}</div>
+                <p aria-hidden="true" className='nav-label'>{item.label}</p>
             </Link>
         </div>
     )
