963267913c698577afd5636e55088470dab5dc26
Ervin <ervinsung@gmail.com>
2025-06-08 19:48:28 +0700

    fix: toggle switch from list item click

--- app/_components/common/ThemeSwitch.js
+++ app/_components/common/ThemeSwitch.js
@@ -1,23 +1,35 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useImperativeHandle, useRef, useState } from "react";
 import ToggleSwitch from "./ToggleSwitch";
 import { IoSunny as LightIcon, IoMoon as DarkIcon } from "react-icons/io5";
 import { useTheme } from "next-themes";
 
-export default function ThemeSwitch() {
+export default function ThemeSwitch({ ref }) {
     const [mounted, setMounted] = useState(false);
     const {resolvedTheme, setTheme} = useTheme();
+    const switchRef = useRef(null);
 
     useEffect(() => {
         setMounted(true);
     }, [])
 
+    useImperativeHandle(ref, () => {
+        return {
+            toggle() {
+                switchRef.current?.click();
+            }
+        }
+    }, [])
+
+    const toggleTheme = (status) => setTheme(status ? 'dark' : 'light')
+
     if(mounted)
     return(
         <ToggleSwitch 
+            ref={switchRef}
             icon={resolvedTheme === 'dark' ? <DarkIcon className="text-sm md:text-base"/> : <LightIcon className="text-sm md:text-base"/>} 
             switchStatus={resolvedTheme === 'dark'}
-            onStatusChange={(status) => setTheme(status ? 'dark' : 'light')}
+            onStatusChange={toggleTheme}
         />
     )
 }
