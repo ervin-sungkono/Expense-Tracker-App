8cb0910ab53a83d9e65cb3fe3961ac43ff9a931e
Ervin <ervinsung@gmail.com>
2025-06-12 19:57:22 +0700

    fix: list item border in light mode

--- app/_components/common/ListItem.js
+++ app/_components/common/ListItem.js
@@ -3,7 +3,7 @@ import { memo } from "react"
 
 function ListItem({ title, description, control, onClick, disabled }) {
     return(
-        <div onClick={onClick} className={`${disabled ? 'pointer-events-none' : ''} cursor-pointer not-last:border-b not-last:border-b-white/20 w-full flex px-4 py-2 active:bg-neutral-300/30 dark:active:bg-neutral-800/30 transition-colors duration-150 ease-in-out`}>
+        <div onClick={onClick} className={`${disabled ? 'pointer-events-none' : ''} cursor-pointer not-last:border-b not-last:border-b-dark/20 dark:not-last:border-b-white/20 w-full flex px-4 py-2 active:bg-neutral-300/30 dark:active:bg-neutral-800/30 transition-colors duration-150 ease-in-out`}>
             <div className={`grow flex flex-col gap-0.5 ${disabled ? 'opacity-40' : ''}`}>
                 <p className="font-semibold text-sm md:text-base text-dark dark:text-white">{title}</p>
                 {description && <p className="text-xs md:text-sm text-dark/80 dark:text-white/80">{description}</p>}
