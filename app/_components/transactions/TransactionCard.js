d70321210ac96c75153cecfdb73f850cfc5919e3
Ervin <ervinsung@gmail.com>
2025-06-12 19:25:50 +0700

    fix: fix transaction card touch effect

--- app/_components/transactions/TransactionCard.js
+++ app/_components/transactions/TransactionCard.js
@@ -43,7 +43,7 @@ function TransactionCard({ transaction }) {
 
     return(
         <div className="relative not-last:border-b border-dark/20 dark:border-white/20 flex items-center">
-            <div {...handlers()} onClick={!isMobile ? () => setShowMenu(true) : null} className="w-full h-full cursor-pointer px-2.5 md:px-4 py-2 flex active:bg-neutral-300/30 active:dark:bg-neutral-800/30 transition-colors duration-150 ease-in-out">
+            <div {...handlers()} onClick={!isMobile ? () => setShowMenu(true) : null} className="w-full h-full cursor-pointer px-2.5 md:px-4 py-2 flex active:bg-neutral-300/30 active:dark:bg-neutral-700/30 transition-colors duration-150 ease-in-out">
                 <div className="w-full flex items-center gap-2.5">
                     <div className="relative shrink-0 w-8 h-8 md:w-10 md:h-10 flex justify-center items-center bg-ocean-blue rounded-full">
                         {category.icon && <Image className="object-contain p-1.5 md:p-2" src={`./category_icons/${category.icon}`} alt="" fill/>}
