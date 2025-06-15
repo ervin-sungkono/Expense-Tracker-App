84600da197f6bd11acda2ba4e869c2a40c158dbf
Ervin <ervinsung@gmail.com>
2025-06-15 17:24:41 +0700

    fix: fix press style on dark mode

--- app/_components/common/Searchbar.js
+++ app/_components/common/Searchbar.js
@@ -23,7 +23,7 @@ export default function SearchBar({ ref, placeholder, maxLength, onSearch }) {
                 maxLength={maxLength}
                 className={`w-full outline-none focus:border-sky-blue px-3 md:px-4 py-2 md:py-2.5 rounded-md bg-transparent border border-deep-blue dark:border-ocean-blue/60 text-dark dark:text-white text-sm transition-colors duration-200 ease-in-out`}
             />
-            <div onClick={handleSearch} className="cursor-pointer absolute right-1 top-1/2 -translate-y-1/2 p-2 rounded-full active:bg-neutral-200/30 dark:active:bg-neutral-700/30 transition-colors duration-150 ease-in-out">
+            <div onClick={handleSearch} className="cursor-pointer absolute right-1 top-1/2 -translate-y-1/2 p-2 rounded-full active:bg-neutral-200/30 dark:active:bg-light/10 transition-colors duration-150 ease-in-out">
                 <SearchIcon size={18}/>
             </div>
         </div>
